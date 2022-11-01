171f50c5a7ce3f1c1ab86d9148c002c2178bec48
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2022-11-01 23:31:59 +0700

    update code lab3

---
+++ lab3/src/App.js
@@ -0,0 +1,13 @@
+import Ex22 from "./components/Ex22";
+import Ex44 from "./components/Ex44";
+import Ex47 from "./components/Ex47";
+
+function App() {
+  return (
+    <div className="App">
+      <Ex47></Ex47>
+    </div>
+  );
+}
+
+export default App;
